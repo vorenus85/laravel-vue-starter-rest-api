204a8526dccd73b02642e3e1084d5754dd3ff693
János Perge <janos.perge@gmail.com>
2026-01-18 17:14:21 +0100

    add meta name="viewport"

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-gray-100">
